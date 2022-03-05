4329fc10c8c57a67aec222784070320b878aee50
pawelmajkut <91788789+pawelmajkut@users.noreply.github.com>
2022-03-05 15:56:44 +0100

    test1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,73 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    class Ulamek
     {
+        // test
+
+        private int licznik { get; set; }
+        private int mianownik { get; set; }
+
+
+        public Ulamek()
+        {
+            
+        }
+
+        public Ulamek(Ulamek previousUlamek)
+        {
+            if (mianownik == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.", nameof(mianownik));
+            }
+            licznik = previousUlamek.licznik;
+            mianownik = previousUlamek.mianownik;
+        }
+
+        public Ulamek(int licznik, int mianownik)
+        {
+            this.licznik = licznik;
+            this.mianownik = mianownik;
+        }
+
+
+        public static Ulamek operator +(Ulamek a) => a;
+        public static Ulamek operator -(Ulamek a) => new Ulamek(-a.licznik, a.mianownik);
+
+        public static Ulamek operator +(Ulamek a, Ulamek b)
+            => new Ulamek(a.licznik * b.mianownik + b.licznik * a.mianownik, a.mianownik * b.mianownik);
+
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+            => a + (-b);
+
+        public static Ulamek operator *(Ulamek a, Ulamek b)
+            => new Ulamek(a.licznik * b.licznik, a.mianownik * b.mianownik);
+
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            if (b.licznik == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new Ulamek(5, 4);
+            var b = new Ulamek(1, 2);
+            Console.WriteLine(-a);   // output: -5 / 4
+            Console.WriteLine(a + b);  // output: 14 / 8
+            Console.WriteLine(a - b);  // output: 6 / 8
+            Console.WriteLine(a * b);  // output: 5 / 8
+            Console.WriteLine(a / b);  // output: 10 / 4
         }
+
+        public override string ToString() => $"{licznik} / {mianownik}";
+        
+
+
+
     }
 }
